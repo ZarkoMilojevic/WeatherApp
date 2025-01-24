0d5ab5c8d2b37a2ac232d42cee3b10ede31c05bc
MSI <zarkomilojevic@yahoo.com>
2025-01-23 19:38:19 -0500

    Application skeleton.

---
+++ Schema.sql
@@ -0,0 +1,40 @@
+-- -----------------------------------------------------
+-- Student name: Zarko Milojevic
+-- Email: zarkomilojevic@yahoo.com
+-- Company: PM Accelerator
+-- Technical Assessment
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Create Schema and User
+-- -----------------------------------------------------
+-- DROP SCHEMA IF EXISTS `weatherapp`;
+-- CREATE SCHEMA IF NOT EXISTS `weatherapp`;
+-- DROP USER IF EXISTS `weatheruser`@`localhost`;
+-- CREATE USER IF NOT EXISTS 'weatheruser'@'localhost' IDENTIFIED BY '1234';
+-- GRANT ALL PRIVILEGES ON `weatherapp`.* TO 'weatheruser'@'localhost';
+
+-- -----------------------------------------------------
+-- Student name: Zarko Milojevic
+-- Create Table `cst8333`.`fuel_consumption_ratings`
+-- -----------------------------------------------------
+-- USE `weatherapp`;
+-- DROP TABLE IF EXISTS `WeatherData`;
+
+-- Create a table to store values
+/*
+CREATE TABLE WeatherData (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    location VARCHAR(255),
+    latitude FLOAT,
+    longitude FLOAT,
+    weather_condition VARCHAR(255),
+    temperature FLOAT,
+    humidity INT,
+    wind_speed FLOAT,
+    date_range VARCHAR(255),
+    date_requested DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+*/
+-- SELECT * FROM WeatherData;
+
